6bf4a37d5fa5be7f3da01f460d9c4bb2a750cbb6
aggdro <agg.drougoutis@gmail.com>
2024-05-15 15:09:04 +0300

    addded connect delete display user pages

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+include 'connect.php';
+if(isset($_GET['deleteid'])){
+    $id=$_GET['deleteid'];
+
+    $sql="delete from `crud` where id=$id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+        echo "Deleted succesfully";
+
+    }
+    else{
+        die(mysqli_error($con));
+    }
+}
+
+
+
+
+
+
+
+
+?>
